1ff6bf408c4f6c99d67135a2c2f6fbe00e886aae
Abraham Park <65786838+parkjutsu@users.noreply.github.com>
2020-12-26 14:14:44 -0800

    Initialize server

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+const port = 8000;
+// Spin up the server
+const server = app.listen(port, listening);
 
-// Setup Server
+// Callback to debug
+function listening() {
+    console.log("server running");
+    console.log(`running on localhost: ${port}`);
+}
+
+// Initialize all route with a callback function
+
+// Callback function to complete GET '/all'
+
+// Post Route
+
+
